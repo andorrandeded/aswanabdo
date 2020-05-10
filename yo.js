77e9df3265b2e1eccb188b8ec72a834cf7e18df6
andorrandeded <59179358+andorrandeded@users.noreply.github.com>
2020-05-10 14:41:35 +0000

    Create yo.js

---
+++ yo.js
@@ -0,0 +1,3 @@
+document.querySelector("body > script:nth-child(7)").innerText="";
+document.querySelector("body > script:nth-child(8)").innerText="";
+document.querySelector("#camera2").innerText="";
